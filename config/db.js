5be8d6559e8730eff2d2b5e222c4c122f4528cb5
3kramz <smahmud1098@gmail.com>
2025-05-24 21:41:24 +0600

    dbconnection udate

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
+// config/db.js
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8qgsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8qgsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 const client = new MongoClient(uri);
 
 async function connectDB() {
   try {
-    // await client.connect();
-    // console.log("MongoDB Connected...");
+    await client.connect(); 
+    console.log("MongoDB Connected successfully");
     return client.db("Quick_Shop");
   } catch (error) {
-    // console.error("MongoDB Connection Failed:", error);
-  
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 }
 
